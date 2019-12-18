2b404684eac839bf6bfd03c49d478a710fa22649
Lucas Dahl <LucasDahl425@yahoo.com>
2019-12-18 12:41:38 -0800

    Initial Commit

---
+++ SwiftUIPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIPractice
+//
+//  Created by Lucas Dahl on 12/18/19.
+//  Copyright © 2019 Lucas Dahl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        // Line break is \n
+        Text("Hello, World! \n\nHello World!")
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
